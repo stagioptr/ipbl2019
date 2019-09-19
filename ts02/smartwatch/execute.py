1ac4474dca172351425d89358d6c957dcbf6db1e
Thiago Cardoso <intelxd@gmail.com>
2019-09-19 20:19:43 -0300

    insert connection retry function

--- ts02/smartwatch/execute.py
+++ ts02/smartwatch/execute.py
@@ -7,18 +7,27 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--mac', required=True, help='Mac address of the device')
 parser.add_argument('-k', '--kafka', required=True, help='Kafka server where the data will be send')
 parser.add_argument('-i', '--interval', help='Interval for reading and sending data', default=30)
-parser.add_argument('-f', '--forever', help='Keep reading data until stop script manually', action='store_true')
+parser.add_argument('-r', '--retry', help='Number of retries if cannot connect to the smartband', default=1)
 args = parser.parse_args()
 
 
-def init(mac, kafka, interval):
-  band = MiBand2(mac, debug=True)
-  band.setSecurityLevel(level="medium")
-  band.authenticate()
+def init(mac, kafka, interval, retry):
+  band = connect(mac, retry)
   while True:
     message = getHeartRate(band)
     sendMessage(kafka, message)
     time.sleep(float(interval))
+
+
+def connect(mac, retry):
+  for (attempt in retry):
+    try:
+      band = MiBand2(mac, debug=True)
+      band.setSecurityLevel(level="medium")
+      band.authenticate()
+      return band
+    except:
+      print("Oops!",sys.exc_info()[0],"occured."))
 
 
 def getHeartRate(band):
@@ -38,4 +47,4 @@ def sendMessage(kafka, message):
 
 if __name__ == '__main__':
   print(args)
-  init(args.mac, args.kafka, args.interval)
+  init(args.mac, args.kafka, args.interval, args.retry)
