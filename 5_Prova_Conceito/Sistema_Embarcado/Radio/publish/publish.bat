31bd6b352bb14a11dbd849d279740bfa0c468ce9
Vitor Cruz <vitorengcruz@gmail.com>
2019-10-15 10:44:39 -0300

    Rádio do sistema embarcado: Adição do publisher do rádio.

---
+++ 5_Prova_Conceito/Sistema_Embarcado/Radio/publish/publish.bat
@@ -0,0 +1,53 @@
+title PUBLISH
+color 1f
+@echo off
+
+cls
+
+if not exist version.ver (
+	(
+		echo Radio
+		echo 0
+	)> version.ver 
+)
+
+< version.ver (
+	set /p driverName=
+	set /p driverVersion=
+)
+
+echo Digite um comentario para a versao %driverVersion% do driver %driverName%: & echo.
+
+set /p texto=Comentario:
+
+if exist publish_log.txt (
+	(
+		echo Versao %driverVersion%:
+		echo %texto% & echo.
+	)>> publish_log.txt
+) else (
+	(
+		echo Controle de versao do driver %driverName% & echo.
+		echo Versao %driverVersion%:
+		echo %texto% & echo.
+	)> publish_log.txt 
+)
+
+set /a "driverNewVersion=%driverVersion% + 1"
+
+(
+	echo %driverName%
+	echo %driverNewVersion%
+)> version.ver
+
+set "dirOutput=versao%driverVersion%"
+
+robocopy "%cd%\..\driver_code" "%cd%/%dirOutput%" /E
+
+cd "%dirOutput%"
+
+(
+	echo Controle de versao do driver %driverName% & echo.
+	echo Versao %driverVersion%:
+	echo %texto% & echo.
+)> publish_log_versao%driverVersion%.txt 
