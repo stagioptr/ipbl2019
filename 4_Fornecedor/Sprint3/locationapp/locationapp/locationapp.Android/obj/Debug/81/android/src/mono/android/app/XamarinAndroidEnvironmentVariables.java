89594c37065d2aab749773eb1578d8346986f386
unknown <carlos.stetner@gmail.com>
2019-11-04 11:34:36 -0300

    locationapp - Carlos Monteiro

---
+++ 4_Fornecedor/Sprint3/locationapp/locationapp/locationapp.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "8ad1a9dc-826d-4552-9708-3d8de9299a0a",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
