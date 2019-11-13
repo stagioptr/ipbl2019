ca515bc2fd3ceaa633e4ef99038b6b0b6b6a9eda
Thiago Cardoso <intelxd@gmail.com>
2019-11-12 22:04:34 -0300

    Add Kafka broker connection

--- 2_Medico/us111/mqtt_consumers/consumers/BaseConsumer.py
+++ 2_Medico/us111/mqtt_consumers/consumers/BaseConsumer.py
@@ -1,32 +1,56 @@
+import json
 import paho.mqtt.client as mqtt
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+LOOP_INTERVAL = .1
 
 class BaseConsumer():
   def __init__(self, topic_in, topic_out, timeout=1, broker='localhost',
-               port=1883, clean_session=True):
+               port=1883, clean_session=True, validator_func=None,
+               sender_func=None):
     self.topic_in = topic_in
     self.topic_out = topic_out
     self.timeout = timeout
     self.broker = broker
     self.port = port
     self.CLIENT = mqtt.Client(clean_session=True)
+    self.KAFKAPRODUCER = KafkaProducer(
+      bootstrap_servers=['localhost:9092'],
+      retries=5,
+      value_serializer=lambda m: json.dumps(m).encode('utf-8')
+    )
+    self.validator_func = validator_func
+    self.sender_func = sender_func
 
   def get_client(self):
     return self.CLIENT
 
-  def listen(self):
+  def connect(self):
     self.CLIENT.connect(self.broker)
     self.CLIENT.subscribe(self.topic_in)
-    self.CLIENT.loop_start()
-    # self.CLIENT.loop_forever(timeout=self.timeout, max_packets=1,
-    #                          retry_first_connection=False)
+    self.CLIENT.on_message = self.on_message
 
-  # def send(self):
-  #   if not self.validate_message(message):
-  #     print(f'Message invalid ({message})')
-  #     return
-  #   pass
+  def listen(self):
+    self.CLIENT.loop(LOOP_INTERVAL)
 
   def validate_message(self, message):
     expected_keys = ['deviceid', 'time', 'value', 'unit']
     results = [expected_key in message for expected_key in expected_keys]
     return False not in results
+
+  def on_message(self, client, userdata, message):
+    payload = json.loads(message.payload.decode("utf-8"))
+    if self.validator_func is None:
+      self.validator_func = self.validate_message
+    if not self.validator_func(payload):
+      print(f'Message invalid ({payload})')
+      return
+    if self.sender_func is None:
+      self.sender_func = self.send
+    self.sender_func(message)
+
+  def send(self, message):
+    print("message received  ",str(message.payload.decode("utf-8")),\
+          "topic",message.topic,"retained ",message.retain)
+    self.KAFKAPRODUCER.send(self.topic_out, str(message.payload.decode("utf-8")))
