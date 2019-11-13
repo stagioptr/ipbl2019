77e938fd0fce515cd61ab7d31ada69142129f82f
Thiago Cardoso <intelxd@gmail.com>
2019-11-12 22:05:16 -0300

    Refactor flow to create and configure consumers

--- 2_Medico/us111/mqtt_consumers/consumer.py
+++ 2_Medico/us111/mqtt_consumers/consumer.py
@@ -2,12 +2,13 @@ from consumers.AccelerometerConsumer import AccelerometerConsumer
 from consumers.HumidityConsumer import HumidityConsumer
 
 consumers = [
-  AccelerometerConsumer('devices/accelerometer', ''),
-  HumidityConsumer('devices/humidity', '')
+  AccelerometerConsumer('devices/accelerometer', 'accelerometer'),
+  HumidityConsumer('devices/humidity', 'humidity')
 ]
+
+for consumer in consumers:
+  consumer.connect()
 
 while True:
   for consumer in consumers:
     consumer.start()
-
-print('test')
