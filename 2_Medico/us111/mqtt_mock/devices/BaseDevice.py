8d8b0b54ccebe60e0e512da41e497ac3f9fed352
Thiago Cardoso <intelxd@gmail.com>
2019-11-12 22:02:46 -0300

    Add docstring to files

--- 2_Medico/us111/mqtt_mock/devices/BaseDevice.py
+++ 2_Medico/us111/mqtt_mock/devices/BaseDevice.py
@@ -10,15 +10,36 @@ class BaseDevice():
     self.ue = ue
 
   def configure(self, broker_info):
+    """Attach the broker info to this device instance
+
+    Arguments:
+        broker_info {dict} -- dict with all broker information
+    """
     self.broker_info = broker_info
 
   def get_value(self):
+    """Get the value from sensor now
+
+    Returns:
+        int -- The value read now from the sensor
+    """
     return random.randrange(self.range_min, self.range_max)
 
   def get_uuid(self):
+    """Return the device
+
+    Returns:
+        string -- this device id
+    """
     return self.uuid
 
   def publish(self, value, formatter_func):
+    """Prepare the value read from the device, and send it to the MQTT broker
+
+    Arguments:
+        value {numeric} -- Value read from the device
+        formatter_func {function} -- Function to put the data in its final form
+    """
     import json
     topic = self.broker_info['topic']
     broker = self.broker_info['broker']
--- 2_Medico/us111/mqtt_mock/publisher.py
+++ 2_Medico/us111/mqtt_mock/publisher.py
@@ -6,7 +6,6 @@ from devices.HeartRate import HeartRate as MockHeartRate
 from devices.Accelerometer import Accelerometer as MockAccelerometer
 
 broker_info = {
-  # 'topic': 'topico/teste',
   'broker': 'localhost',
   'port': 1883,
 }
@@ -14,6 +13,14 @@ broker_info = {
 init_devices = list()
 
 def start(sleep_interval, execution_count=-1):
+  """Iterate endlessly through the devices
+  
+  Arguments:
+      sleep_interval {int} -- time in seconds the script will wait for the devices to send data again
+  
+  Keyword Arguments:
+      execution_count {int} -- how many times the devices will be iterated (default: {-1})
+  """
   while True:
     for device in devices():
       device.publish()
@@ -23,18 +30,42 @@ def start(sleep_interval, execution_count=-1):
     time.sleep(sleep_interval)
 
 def devices():
+  """Initialize devices to be used on the script execution
+  
+  Returns:
+      list -- list containing all instantiated devices
+  """
   if len(init_devices) == 0:
     init_devices.append(generate_device('humidity', (40, 90), 'HR'))
     init_devices.append(generate_device('temperature', (15, 35), 'C'))
     init_devices.append(generate_device('heartrate', (60, 110), 'bpm'))
     init_devices.append(generate_device('accelerometer', (0, 10), 'km/h'))
+    init_devices.append(generate_device('accelerometer', (0, 10), 'km/h'))
   return init_devices
 
 def generate_uuid(preffix):
+  """Generates a random uuid and append the prefix to it
+  
+  Arguments:
+      preffix {string} -- String to be added to the start of the generated id
+  
+  Returns:
+      string -- random device id with its preffix
+  """
   id = uuid.uuid4()
   return f"{preffix}_{id}"
 
 def generate_device(device_type, range_tuple, ue):
+  """Helps to instantiate new devices, according to its type and configuration
+  
+  Arguments:
+      device_type {string} -- string to identify which device will be generated
+      range_tuple {tuple(int)} -- tuple with the lower and higher values the device can generate
+      ue {string} -- engineering unit of the generated value
+  
+  Returns:
+      implementation of BaseDevice -- an instantiated device
+  """
   switcher = {
     'humidity': MockHumidity,
     'temperature': MockTemperature,
@@ -51,6 +82,14 @@ def generate_device(device_type, range_tuple, ue):
   return device
 
 def generate_topic(topic):
+  """Builds the broker info dict, with the correct topic for the device
+  
+  Arguments:
+      topic {string} -- name of the topic for where the device will send its data
+  
+  Returns:
+      dict -- dict containing the broker address, port and topic
+  """
   import copy
   new_info = copy.deepcopy(broker_info)
   new_info['topic'] = topic
