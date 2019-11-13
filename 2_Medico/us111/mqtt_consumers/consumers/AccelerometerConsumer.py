7d3732ea6aef118e0c8b3bb8b6d60ee04dbd5696
Thiago Cardoso <intelxd@gmail.com>
2019-11-12 22:04:50 -0300

    Remove unecessary code after refactor

--- 2_Medico/us111/mqtt_consumers/consumers/AccelerometerConsumer.py
+++ 2_Medico/us111/mqtt_consumers/consumers/AccelerometerConsumer.py
@@ -5,16 +5,6 @@ class AccelerometerConsumer(BaseConsumer.BaseConsumer):
                port=1883, clean_session=True):
     super(AccelerometerConsumer, self).__init__(topic_in, topic_out, timeout,
                                                 broker, port, clean_session)
-    self.configure()
-
-  def configure(self):
-    super().get_client().on_message = self.on_message
 
   def start(self):
     super().listen()
-
-  def on_message(self, client, userdata, message):
-    print("message received  ",str(message.payload.decode("utf-8")),\
-          "topic",message.topic,"retained ",message.retain)
-    if message.retain==1:
-      print("This is a retained message")
