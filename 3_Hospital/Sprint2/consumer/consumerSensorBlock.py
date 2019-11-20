b90d9df195dcb78697d6797c4f8d61d710e5ebc0
alvarengaricardo <alvarenga.r.ricardo@gmail.com>
2019-11-20 00:19:26 -0300

    consumer blockchain final

--- 3_Hospital/Sprint2/consumer/consumerSensorBlock.py
+++ 3_Hospital/Sprint2/consumer/consumerSensorBlock.py
@@ -1,7 +1,17 @@
-from utils import insert_data
+from utilsblock import insert_data
 from pymongo import MongoClient
 from kafka import KafkaConsumer
 from datetime import datetime
+
+# mongo =====
+# Connect
+cliente = MongoClient('mongodb://stagioptr:ipbl2019@localhost:27017/')
+
+# Selecionando um Banco
+banco = cliente.stagioptr
+
+# collection
+album = banco.sensor
 
 # kafka======
 consumer = KafkaConsumer('sensor')
