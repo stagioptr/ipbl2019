051459e523e399f2c1f2cc4cf5cd2b9644cc258d
vitorengcruz <vitorengcruz@gmail.com>
2020-05-08 21:56:29 -0300

    Modulo embarcado - Add Kafka script

---
+++ 5_Prova_Conceito/Sistema_Embarcado/Radio/raspberry_pi/sendToKafka_V2.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+import time
+import serial
+
+
+ser = serial.Serial(
+	port='/dev/ttyACM0',
+	baudrate = 115200,
+	parity=serial.PARITY_NONE,
+	stopbits=serial.STOPBITS_ONE,
+	bytesize=serial.EIGHTBITS,
+	timeout=None
+	)
+
+#initialize kafka producer
+producer = KafkaProducer(bootstrap_servers=['34.68.34.160:9092'],
+	value_serializer=lambda x:
+	dumps(x).encode('utf-8'))
+
+while 1:
+	start=ser.readline().rstrip()
+	if (start == "Start"):
+		freeFall = ser.readline().rstrip()
+		deviceID = ("200.234.583-05")
+		temperature = ser.readline().rstrip()
+		accelerometer = ser.readline().rstrip()
+		gyro = ser.readline().rstrip()
+		end = ser.readline().rstrip()
+		if(end == "End"):
+			now = datetime.now()
+			dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+			documento = {
+				'id' : deviceID,
+				'dataHora' : dt_string,
+				'temperatura' : float(temperature),
+				'umidade' : 40,
+				'acelerometro' : accelerometer,
+				'giroscopio' : gyro
+			}
+			producer.send('sensor', documento)
+			print(documento)
+			if(freeFall == "1"):
+				emergencia = {
+					'id' : deviceID,
+					'dataHora' : dt_string
+				}
+				producer.send('emergency', emergencia)
+				print(emergencia)
